c7c7fc828fa04908c7ee46f3e23fe43e35b2a3ea
thibault.dulon <thibault.dulon@gmail.com>
2014-06-21 23:56:26 +0200

    no message

--- Library/Core/UrlRewriting.php
+++ Library/Core/UrlRewriting.php
@@ -30,7 +30,7 @@
     		$this->_repositoryManager = new RepositoryManager($pdo, $this->_session->Read("lang"));
     		
     		$this->_langRepository = $this->_repositoryManager->get('Lang');
-    		$this->_rewrittingUrlRepository = $this->_repositoryManager->get('RewrittingUrl');
+    		$this->_rewrittingUrlRepository = $this->_repositoryManager->get('Rewrittingurl');
     		$this->_routeUrlRepository = $this->_repositoryManager->get('Routeurl');
     	}
 
