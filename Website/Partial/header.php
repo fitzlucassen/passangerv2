0acd5617fa9ca6aab56915dade29945a5aa598fd
thibault.dulon <thibault.dulon@gmail.com>
2014-06-22 03:20:00 +0200

    theme

--- Website/Partial/header.php
+++ Website/Partial/header.php
@@ -6,10 +6,11 @@
     <div id="main-navigation-container">
         <nav id="main-navigation">
             <ul>
-                <li><a href="javascript:void(0)" class="first nav-link">Home</a></li>
-                <li><a href="javascript:void(0)" class="nav-link">Menu Item</a></li>
-                <li><a href="javascript:void(0)" class="nav-link">Menu Item</a></li>
-                <li><a href="javascript:void(0)" class="last nav-link">Menu Item</a></li>
+                <li><a href="javascript:void(0)" class="first nav-link"><i class="fa fa-home fa-fw"></i><?php echo _('Accueil'); ?></a></li>
+                <li><a href="javascript:void(0)" class="nav-link"><i class="fa fa-music fa-fw"></i><?php echo _('Bio'); ?></a></li>
+                <li><a href="javascript:void(0)" class="nav-link"><i class="fa fa-rss fa-fw"></i><?php echo _('Actualités'); ?></a></li>
+                <li><a href="javascript:void(0)" class="last nav-link"><i class="fa fa-image fa-fw"></i><?php echo _('Media'); ?></a></li>
+                <li><a href="javascript:void(0)" class="last nav-link"><i class="fa fa-envelope-o fa-fw"></i><?php echo _('Contact'); ?></a></li>
             </ul>
         </nav>
     </div>
